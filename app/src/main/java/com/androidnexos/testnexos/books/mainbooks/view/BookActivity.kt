e70c4aabc914354bebc1f76423297eb8ce8c8cb9
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-02 17:22:55 -0500

    Se inicializa variable de viewModel y se agrega función que obtiene los libros por primera vez

--- app/src/main/java/com/androidnexos/testnexos/books/mainbooks/view/BookActivity.kt
+++ app/src/main/java/com/androidnexos/testnexos/books/mainbooks/view/BookActivity.kt
@@ -2,15 +2,18 @@ package com.androidnexos.testnexos.books.mainbooks.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.androidnexos.testnexos.R
 import com.androidnexos.testnexos.books.mainbooks.model.Book
 import com.androidnexos.testnexos.books.mainbooks.view.adapter.BookAdapter
+import com.androidnexos.testnexos.books.mainbooks.viewmodel.BookViewModel
 import com.androidnexos.testnexos.databinding.ActivityMainBinding
 
 class BookActivity : AppCompatActivity() {
     private var mBookBinding: ActivityMainBinding? = null
     private var mBookAdapter: BookAdapter? = null
+    private var mViewModel: BookViewModel? = null
 
     private var dataBookList = ArrayList<Book>()
 
@@ -19,13 +22,17 @@ class BookActivity : AppCompatActivity() {
         mBookBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = mBookBinding?.root
         setContentView(view)
+        initializeWidgets()
     }
 
     private fun initializeWidgets() {
+        mViewModel = ViewModelProvider(this)[BookViewModel::class.java]
         mBookAdapter = BookAdapter(this, dataBookList)
         with(mBookBinding?.listBooks) {
             this?.layoutManager = LinearLayoutManager(this@BookActivity)
             this?.adapter = mBookAdapter
         }
+
+        mViewModel?.getBooks("mongodb")
     }
 }

96754740bdf1fd208543b0b2baceaa0a098ec4d3
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-02 00:12:07 -0500

    Se agrega función que permite obtener la data de la api y se pasa a lista mutable que va permitir notifica el cambio de datos

--- app/src/main/java/com/androidnexos/testnexos/books/mainbooks/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/androidnexos/testnexos/books/mainbooks/viewmodel/BookViewModel.kt
@@ -1,4 +1,33 @@
 package com.androidnexos.testnexos.books.mainbooks.viewmodel
 
-class BookViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androidnexos.testnexos.books.api.ApiCallbackHelper
+import com.androidnexos.testnexos.books.api.BookRepository
+import com.androidnexos.testnexos.books.mainbooks.model.Book
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class BookViewModel: ViewModel() {
+    private val mBookRepository = BookRepository()
+
+    val showListBooksLiveDate = MutableLiveData<Book>()
+
+    fun getBooks(tittle: String) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                mBookRepository.getBooks(tittle, object : ApiCallbackHelper.GeneralCallback<Book> {
+                    override fun onSuccess(response: Book?) {
+                        showListBooksLiveDate.value = response
+                    }
+
+                    override fun onFailure() {
+                    }
+
+                })
+            }
+        }
+    }
 }

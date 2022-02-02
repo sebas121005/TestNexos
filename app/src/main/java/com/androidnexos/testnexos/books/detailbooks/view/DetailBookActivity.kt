428874c9dd59a5c2d6e8e2b75361e3d33a4757dc
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-02 18:51:18 -0500

    Se agrega vista para mostrar detalle de los libros

---
+++ app/src/main/java/com/androidnexos/testnexos/books/detailbooks/view/DetailBookActivity.kt
@@ -0,0 +1,12 @@
+package com.androidnexos.testnexos.books.detailbooks.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androidnexos.testnexos.R
+
+class DetailBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_book)
+    }
+}

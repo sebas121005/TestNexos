173daa329d2e60fe47994ba6a8c9259e94e58592
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-01 23:39:10 -0500

    Se implementa consumo de endpoint en la función de getbooks ejecutando los callback en caso de exito o de error

--- app/src/main/java/com/androidnexos/testnexos/books/api/BookRepository.kt
+++ app/src/main/java/com/androidnexos/testnexos/books/api/BookRepository.kt
@@ -2,6 +2,9 @@ package com.androidnexos.testnexos.books.api
 
 import com.androidnexos.testnexos.BuildConfig
 import com.androidnexos.testnexos.books.mainbooks.model.Book
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class BookRepository {
@@ -11,7 +14,18 @@ class BookRepository {
         webService = RetrofitManager().createWebService(BuildConfig.URL_SERVER)
     }
 
-    fun getBooks(callbackHelper: ApiCallbackHelper.GeneralCallback<Book>) {
+    fun getBooks(tittle: String, callbackHelper: ApiCallbackHelper.GeneralCallback<Book>) {
+        webService?.getBooks(tittle)?.enqueue(object: Callback<Book> {
+            override fun onResponse(call: Call<Book>, response: Response<Book>) {
+                if (response.isSuccessful) {
+                    response.body()?.let { callbackHelper.onSuccess(it) }
+                }
+            }
 
+            override fun onFailure(call: Call<Book>, t: Throwable) {
+                callbackHelper.onFailure()
+            }
+
+        })
     }
 }

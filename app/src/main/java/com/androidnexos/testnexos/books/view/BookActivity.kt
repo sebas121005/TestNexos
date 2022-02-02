0b85af36adfcb253b9edde7348980b53b585f3bd
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-01 22:44:06 -0500

    Se modifica nombre de la activity principal y se agrega al paquete de correspondiente

--- app/src/main/java/com/androidnexos/testnexos/books/view/BookActivity.kt
+++ app/src/main/java/com/androidnexos/testnexos/books/view/BookActivity.kt
@@ -1,9 +1,10 @@
-package com.androidnexos.testnexos
+package com.androidnexos.testnexos.books.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.androidnexos.testnexos.R
 
-class MainActivity : AppCompatActivity() {
+class BookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

8a762b71561dc8ddb11178ce33b9b09f3d5e35f8
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-02-02 19:47:16 -0500

    Se agrega configuración de la toolbar junto con su acción al pulsar el botón de atrás

--- app/src/main/java/com/androidnexos/testnexos/books/detailbooks/view/DetailBookActivity.kt
+++ app/src/main/java/com/androidnexos/testnexos/books/detailbooks/view/DetailBookActivity.kt
@@ -2,11 +2,34 @@ package com.androidnexos.testnexos.books.detailbooks.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.androidnexos.testnexos.R
 
 class DetailBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_book)
+        initializeWidget()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+
+
+    fun initializeWidget() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.detalle_libro)
     }
 }
